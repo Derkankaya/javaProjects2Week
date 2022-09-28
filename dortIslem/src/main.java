2247f1a1a229c116b82d291d8d43531e54e9a8e0
Derkankaya <114098020+Derkankaya@users.noreply.github.com>
2022-09-28 18:01:18 +0300

    javaProjects

---
+++ dortIslem/src/main.java
@@ -0,0 +1,7 @@
+public class main {
+    public static void main(String[] args) {
+        DortIslem dortIslem = new DortIslem();
+        int sonuc =dortIslem.Topla(3,2);
+        System.out.println(sonuc);
+    }
+}
